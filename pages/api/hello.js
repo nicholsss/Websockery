2542b7b93d207658fda91a54c0bfffde1bdc0094
nicholsss <mickyaw1995@gmail.com>
2022-10-14 12:54:05 +0300

    first init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Server } from "socket.io";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function SocketHandler(req, res) {
+  // It means that socket server was already initialised
+  if (res.socket.server.io) {
+    console.log("Already set up");
+    res.end();
+    return;
+  }
+
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
+
+  const onConnection = (msg, socket) => {
+    socket.broadcast.emit(msg)
+  };
+
+  // Define actions inside
+  io.on("connection", onConnection);
+
+  console.log("Setting up socket");
+  res.end();
 }
